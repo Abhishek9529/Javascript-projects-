862528fd6a54943d72a296381229ec13b76c13c8
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-02-10 22:08:32 +0530

    modified file

--- Typing Speed/script.js
+++ Typing Speed/script.js
@@ -1,61 +1,65 @@
-let para = document.querySelector('#para')
-let submit = document.querySelector('#submit')
+let showParagraph = document.querySelector('#para')
 let result = document.querySelector('#result')
-let timerEl = document.querySelector('.timer')
+let displayCurrentTime = document.querySelector('.timer')
 let totalTimeEl = document.querySelector('#totalTime')
-let timer = 0
+let timer = 0;
+let interval;
 
-let setense =''
-  const url = 'https://api.freeapi.app/api/v1/public/quotes/quote/random';
-  const options = { method: 'GET', headers: { accept: 'application/json' } };
+let paragraph = ''
+const url = 'https://api.freeapi.app/api/v1/public/quotes/quote/random';
+const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-  async function getdata()
-  {
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      const contentData = await data.data
-      const content = contentData.content
-     para.textContent = content
-     setense = content
-    //  console.log(content);
-    } catch (error) {
-      console.error(error);
+const getdata = async () => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (data && data.data && data.data.content) {
+      paragraph = data.data.content
+      showParagraph.textContent = paragraph
     }
+    else{
+      console.log('Paragraph Loading...');
+    }
+  } catch (error) {
+    console.error(error);
   }
-  getdata()
-  
-  const form = document.querySelector('form');
-  form.addEventListener('submit',(e)=>{
-    
-    e.preventDefault();
-    clearInterval(interval);
-    totalTimeEl.textContent = timer - 1
-    //const data = new FormData(form)
-   let content = document.getElementById('inputtext').value
-   console.log(content)
-   if (content === setense) {
-     result.textContent = 'nice type'
-     result.style.color = 'green'
-     console.log('nice type')
-   }
-   else{
-     result.textContent = 'bad type'
-     result.style.color = 'red'
-     console.log('bad type')
-   }
-  })
+}
+getdata()
 
-  const interval = setInterval(()=>{
-    
-  if (timer >= 0){
-  timerEl.textContent = timer
-   timer++;
-  timerEl.style.color = 'red'
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+  clearInterval(interval);
+
+  totalTimeEl.textContent = timer
+
+  let inputText = document.getElementById('inputtext').value.trim().toLowerCase()
+  let originalText = paragraph.trim().toLowerCase()
+
+  if (inputText === originalText) {
+    result.textContent = 'Nice type'
+    result.style.color = 'green'
   }
-  },1000)
- 
- /** 
-  function  clearinterval () {
-    clearinterval(interval)
-  }**/
+  else {
+    result.textContent = 'Bad type'
+    result.style.color = 'red'
+  }
+
+})
+
+const startTime = () => {
+  timer = 0;
+  // displayCurrentTime.textContent = timer
+  // displayCurrentTime.style.color = 'red'
+
+  interval = setInterval(() => {
+    displayCurrentTime.textContent = timer
+    timer++;
+    displayCurrentTime.style.color = 'red'
+  }, 1000)
+};
+
+document.getElementById('inputtext').addEventListener('focus', startTime)
+
