9e54f549c28f07e27a3749009db919835cf4481c
Abhishek Chavhan <abhishekchavhan61047@gmail.com>
2025-02-18 15:57:43 +0530

    Update script.js

--- Typing Speed/script.js
+++ Typing Speed/script.js
@@ -1,11 +1,13 @@
-let showParagraph = document.querySelector('#para');
+let showParagraph = document.querySelector('#originalText');
 let result = document.querySelector('#result');
 let displayCurrentTime = document.querySelector('.timer');
 let totalTimeEl = document.querySelector('#totalTime');
 let accuracyEl = document.querySelector('#accuracy');
 let outputEl = document.querySelector('.output')
+let typingSpeedEl = document.querySelector('#typingSpeed');
 let timer = 0;
 let interval;
+let sTime, eTime;
 
 let paragraph = ''
 const url = 'https://api.freeapi.app/api/v1/public/quotes/quote/random';
@@ -16,15 +18,16 @@ const getdata = async () => {
     const response = await fetch(url, options);
     const data = await response.json();
 
-    if (data && data.data && data.data.content) {``
+    if (data && data.data && data.data.content) {
+
       paragraph = await data.data.content
-      if(!showParagraph){
+      if (!showParagraph) {
         console.log('paragraph not found...');
-        
+
       }
       showParagraph.textContent = paragraph
     }
-    else{
+    else {
       console.log('Paragraph Loading...');
     }
   } catch (error) {
@@ -36,54 +39,54 @@ getdata()
 const form = document.querySelector('form');
 
 
-if(form){
+if (form) {
   form.addEventListener('submit', (e) => {
-
+    let correctWord = 0;
+    let totalWord;
+    eTime = Date.now();
     e.preventDefault();
     clearInterval(interval);
-  
-    totalTimeEl.textContent = timer
-  
-    let inputText = document.getElementById('inputtext').value.trim().toLowerCase()
-    let originalText = paragraph.trim().toLowerCase()
-    // let output = inputText
 
-    
-    // for(let i = 0; i < originalText.length; i++){
-      
-      
-    //   if(output[i] != originalText[i]){
-    //     console.log(originalText[i],' =  ', inputText[i] , 'wrong');
-    //   }else{
-    //     console.log(originalText[i],' =  ', inputText[i]);
-    //   }
-      
-    // }
- 
-    if (inputText === originalText) {
-      result.textContent = 'Nice type'
-      result.style.color = 'green'
+
+    let inputText = document.getElementById('inputtext').value.trim().toLowerCase().split(" ").filter(word => word !== "")
+
+    let originalText = paragraph.trim().toLowerCase().split(" ").filter(word => word !== "")
+    totalWord = originalText.length;
+
+
+    let output = "";
+    for (let i = 0; i < originalText.length; i++) {
+
+      if (inputText[i] === originalText[i]) {
+        output += inputText[i] + " ";
+        correctWord += 1;
+      }
+      else {
+        output += `<span id = "red">${inputText[i]}</span>` + " ";
+      }
     }
-    else {
-      result.textContent = 'Bad type'
-      result.style.color = 'red'
-    }
+    outputEl.innerHTML = output;
+    let totalTime = ((eTime - sTime) / 1000).toFixed(2)
+
+    let wpm = (inputText.length / totalTime) * 60;
+    typingSpeedEl.textContent = wpm.toFixed(2);
+
+    totalTimeEl.textContent = totalTime;
+    accuracyEl.textContent = ((correctWord / totalWord) * 100).toFixed(2)
+
   })
-  
+
 }
-else{
+else {
   console.log('form not found...');
 }
 
 const startTime = () => {
-  timer = 0;
-
-  interval = setInterval(() => {
-    displayCurrentTime.textContent = timer
-    timer++;
-    displayCurrentTime.style.color = 'red'
-  }, 1000)
+  sTime = Date.now()
 };
 
 document.getElementById('inputtext').addEventListener('focus', startTime)
+
+
+
 
